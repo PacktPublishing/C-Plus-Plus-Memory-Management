2da36198c38137e02ec2ecc05498a0d977b44f50
patricer <patricer@gmail.com>
2024-06-03 10:45:45 -0400

    Add files via upload
    Removed a useless static assertion (leftover from previous versions of the example)

--- chapter3/chapter-3-bit_cast.cpp
+++ chapter3/chapter-3-bit_cast.cpp
@@ -1,11 +1,9 @@
-// also available live: https://wandbox.org/permlink/PamPZDAir10XqTnq
+// also available live: https://wandbox.org/permlink/mhY2nLvl37Jwzf6m
 
 #include <bit>
 struct A { int a; double b; };
 struct B { unsigned int c; double d; };
 int main() {
-   // suppose this for the sake of this example
-   static_assert(sizeof(int) == sizeof(float));
    constexpr A a{ 3, 3.5 }; // ok
    constexpr B b = std::bit_cast<B>(a); // Ok
    static_assert(a.a == b.c && a.b == b.d); // Ok
