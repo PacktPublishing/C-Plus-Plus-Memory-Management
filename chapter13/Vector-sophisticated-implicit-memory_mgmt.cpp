964776c137e003476b7621d8ea355cebdc49b31f
patricer <patricer@gmail.com>
2026-08-14 19:41:33 -0400

    Fixed bug in erase()

--- chapter13/Vector-sophisticated-implicit-memory_mgmt.cpp
+++ chapter13/Vector-sophisticated-implicit-memory_mgmt.cpp
@@ -1,4 +1,4 @@
-// also available live: https://wandbox.org/permlink/PfmioAXubSIFlyKi
+// also available live: https://wandbox.org/permlink/lkDAqlZwguWDuflv
 
 #include <cstddef>
 #include <algorithm>
@@ -235,7 +235,7 @@ public:
       iterator pos_ = const_cast<iterator>(pos);
       if (pos_ == end()) return pos_;
       std::copy(std::next(pos_), end(), pos_);
-      *std::prev(end()) = {};
+      std::destroy_at(std::prev(end()));
       --nelems;
       return pos_;
    }
